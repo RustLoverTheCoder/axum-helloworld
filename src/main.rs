e82cb6247471765d4bcc8ac77e7b0c2e0d82a5f7
maxiang <maxiang@haoshuo.com>
2022-06-07 12:36:09 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> String {
+    let target = std::env::var("TARGET").unwrap();
+    target.to_string()
+}
